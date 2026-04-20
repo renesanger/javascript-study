f2caaa2cc91bcad799ff19d492c3aa23fd62a316
renesanger <renejsanger@gmail.com>
2026-04-20 13:00:33 -0400

    Enhance destructuring-exercises.js with comments
    Added comments explaining destructuring of user and salaries objects.

--- destructuring-exercises.js
+++ destructuring-exercises.js
@@ -1,3 +1,8 @@
+/*
+  Destructuring an object called user.
+  give specific variable names values from a user object
+*/
+
 let user = {
   name: "Rene",
   years: 27
@@ -7,6 +12,12 @@ let {name: name, years: age, isAdmin = false} = user;
 console.log(name);
 console.log(age);
 console.log(isAdmin);
+
+/*
+  Destructuring salaries objects,
+  using destructuring in a foreach loop to iterate through a dictionary of salaries.
+  return the name of the employee with the highest salary
+*/
 
 let salaries = {
   "John" : 100,
@@ -24,8 +35,11 @@ function topSalary(salaries) {
       maxName = name;
     }
   }
-
   return maxName;
 }
+
+
+
+
 
 console.log(topSalary(salaries));
