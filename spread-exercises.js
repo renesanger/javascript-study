d0c77624bc4bc8991941d04406291969216fd132
renesanger <renejsanger@gmail.com>
2026-04-21 12:06:11 -0400

    Update spread-exercises.js

--- spread-exercises.js
+++ spread-exercises.js
@@ -5,14 +5,24 @@
 //Create combined with all 6 numbers using spread
 const a = [1,2,3];
 const b = [4,5,6];
+const combined = [...a, ...b];
 
 //Build setlist = intro, verse, chorus, outro = in order
 const first = ["intro"];
 const last = ["outro"];
 const middle = ["verse", "chorus"];
+const setlist = [...first, ...middle, ...last];
 
-//Create config by merging bothe - overrides should win on conflicts
+//Create config by merging both - overrides should win on conflicts
 const defaults = { volume:50, muted: false };
 const overrides = { volume: 80 };
+const config = [...defaults, ...overrides];
 
-//creat
+//create updatedArtist - same fields, but add verified: true
+const artist = { name: "R.J.", genre: "hip-hop" };
+const updatedArtists = { ...artist, verified: true };
+
+//Use Math.max() with spread to get the largest number. Store it in biggest.
+const nums = [3 ,7, 2, 9, 1];
+const biggest = Math.max(...nums);
+
