478c4521b2b20647c087fdfde0f7cda9379c987d
renesanger <renejsanger@gmail.com>
2026-04-23 13:46:56 -0400

    Update async-await-exercise.js

--- async-await-exercise.js
+++ async-await-exercise.js
@@ -32,6 +32,12 @@ async function loadJson(url) {
 
 loadJson('https://javascript.info/no-such-user.json');
 
+/*
+  The best way to format the async function is this way.
+
+  the catch is done outside of the async function so that the person calling it can decide what to do with the promise.
+  The reason why is because of a concept called separation of concerns. the function is responsible for returning the response and not handling the error.
+*/
 async function loadJson(url) {
   let response = await fetch(url);
   if(response.status == 200) {
